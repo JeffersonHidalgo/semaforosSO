78384e964a2578650b22a6351bb825a26956e747
JeffersonHidalgo <jeffersonhidalgo853@gmail.com>
2023-04-05 16:24:01 -0400

    codes

---
+++ semaforos codes/studio.h
@@ -0,0 +1,7 @@
+#ifndef STUDIO_H
+#define STUDIO_H
+
+void print_message();
+
+#endif
+
---
+++ semaforos codes/time.h
@@ -0,0 +1,9 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <time.h>
+
+void get_current_time();
+void delay_seconds(int seconds);
+
+#endif
